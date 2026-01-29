90d11c1e45789694a485655bc9b83dba64a1f5c0
Frade11 <gurdisartemii@gmail.com>
2026-01-29 16:45:19 +0200

    created function for posts line size

--- pages/home.php
+++ pages/home.php
@@ -100,11 +100,25 @@
                       $stmt->close();
                       $conn->close();
                     ?>
-                    <!-- <div class="line_post"><img src="/uploads/post_69714b2ed400f.jpg" alt=""></div>
-                    <div class="line_post"><img src="/uploads/post_69714b2ed400f.jpg" alt=""></div>
-                    <div class="line_post"><img src="/uploads/post_69714b2ed400f.jpg" alt=""></div>
-                    <div class="line_post"><img src="/uploads/post_69714b2ed400f.jpg" alt=""></div>
-    -->
+
+                <script>
+                    function adjustPosts(){
+                        const posts = document.querySelectorAll('.line_post');
+                        let showNumber = 6;
+
+                        if(window.innerWidth < 1400) showNumber = 5; 
+                        if(window.innerWidth < 1200) showNumber = 4; 
+                        if(window.innerWidth < 900) showNumber = 3; 
+                        if(window.innerWidth < 768) showNumber = 3; 
+                        if(window.innerWidth < 500) showNumber = 2; 
+
+                        posts.forEach((post, index) =>{
+                            post.style.display = index < showNumber ? '' : 'none';
+                        })
+                    }
+                    window.addEventListener("load",adjustPosts);
+                    window.addEventListener("resize",adjustPosts);
+                </script>
 
                 </div>
 
