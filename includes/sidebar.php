1b4e83e3c98aa6a1b21aa3d1743ff6e6597132d6
Frade11 <gurdisartemii@gmail.com>
2026-01-29 15:29:56 +0200

    created redirection when logo clicked

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-solid-chubby/css/uicons-solid-chubby.css'>
   <div class="sidebar">
         <div class="top-links">
-            <img src="/assets/images/logo.jpg" alt="logo">
+            <img src="/assets/images/logo.jpg" class="logoImg" alt="logo" >
             <a href="home.php" id = "home" >Home</a>
             <a href="posts.php" id = "posts">Posts</a>
             <a href="trends.php" id = "trends">Trends</a>
@@ -14,24 +14,4 @@
         </div>
     </div>
 
-    <script>
-        const currentPage = window.location.pathname;
-        const pageName = currentPage.split('/').pop();
-        
-
-        const homeBtn = document.getElementById('home');
-        const postsBtn = document.getElementById('posts');
-        const trendsBtn = document.getElementById('trends');
-        const pinsBtn = document.getElementById('pins');
-        const createBtn = document.getElementById('create');
-        console.log(homeBtn);
-
-        switch(pageName){
-            case 'home.php': homeBtn.classList.add("active"); break;
-            case 'posts.php': postsBtn.classList.add("active"); break;
-            case 'trends.php': trendsBtn.classList.add("active"); break;
-            case 'pins.php': pinbsBtn.classList.add("active"); break;
-            case 'create.php': createBtn.classList.add("active"); break;
-            default:homeBtn.classList.add("active");
-        }
-    </script>
+   <script src="/assets/js/currentPage.js"></script>
