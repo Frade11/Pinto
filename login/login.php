596b913de3bb8dde8409b520dd97eed5e0e0a43f
Frade11 <gurdisartemii@gmail.com>
2026-01-30 20:58:39 +0200

    created refister form styles

--- login/login.php
+++ login/login.php
@@ -8,13 +8,24 @@
 </head>
 <body>
      <div class="form-container">
-        <div class="form_box active" id="login-form">
+        <div class="form_box" id="login-form">
             <form action="login_registration.php" method="POST">
                 <h2>Login</h2>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <button type="submit" name="login">Login</button>
                 <p>Dont have an account? <a href="" onclick="showForm('register-form')">Register</a></p>
+            </form>
+        </div>
+
+        <div class="form_box active" id="register-form">
+            <form action="login_registration.php" method="POST">
+                <h2>Register</h2>
+                <input type="text" name="name" placeholder="Name" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit" name="register">Register</button>
+                <p>Already have an account? <a href="" onclick="showForm('login-form')">Login</a></p>
             </form>
         </div>
         
