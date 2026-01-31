3f541b2bec7dbe9a79effb401f8feca12b067856
Frade11 <gurdisartemii@gmail.com>
2026-01-31 18:39:27 +0200

    added icons for sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,12 +1,14 @@
-   <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-solid-chubby/css/uicons-solid-chubby.css'>
+<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-solid-chubby/css/uicons-solid-chubby.css'>
+<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+
   <div class="sidebar">
         <div class="top-links">
             <img src="../assets/images/logo.jpg" class="logoImg" alt="logo" >
-            <a href="home.php" id = "home" >Home</a>
-            <a href="posts.php" id = "posts">Posts</a>
-            <a href="trends.php" id = "trends">Trends</a>
-            <a href="pins.php" id = "pins">Pins</a>
-            <a href="create.php" id = "create">Create</a>
+            <a href="home.php" id = "home" ><i class="fi fi-rr-home"></i>Home</a>
+            <a href="posts.php" id = "posts"><i class="fi fi-rr-camera"></i>Posts</a>
+            <a href="trends.php" id = "trends"><i class="fi fi-rr-arrow-trend-up"></i>Trends</a>
+            <a href="pins.php" id = "pins"><i class="fi fi-rr-bookmark"></i>Pins</a>
+            <a href="create.php" id = "create"><i class="fi fi-rr-multiple"></i>    Create</a>
         </div>
 
         <div class="bottom-links">
