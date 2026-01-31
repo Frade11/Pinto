219949b8865ad182d46a012d635316fd1fbb3053
Frade11 <gurdisartemii@gmail.com>
2026-01-31 21:22:40 +0200

    created user info overlay and added function to delete cockies when logout

--- assets/js/mainScripts.js
+++ assets/js/mainScripts.js
@@ -3,3 +3,21 @@ exploreBtns.forEach(btn => {
     btn.addEventListener('click',()=>{
     window.location.href = 'posts.php'
 })})
+
+const userIcon = document.getElementById('userIcon');
+const userOverlay = document.getElementById('userOverlay');
+
+userIcon.addEventListener('click', function(){
+    if(userOverlay.style.display === 'block'){
+        userOverlay.style.display = 'none';
+    }else{
+         userOverlay.style.display = 'block';
+    }
+});
+
+const logOutBtn = document.getElementById('logOut');
+logOutBtn.addEventListener('click', () => {
+    window.location.href='../login/logout.php';
+})
+
+
--- includes/topnav.php
+++ includes/topnav.php
@@ -6,6 +6,26 @@
         </div>
         <div class="top-icons">
             <div class="notifications"><i class="fi fi-rs-bell-notification-social-media"></i></div>
-            <img src="../assets/images/logo.jpg" alt="user" class="userIcon" onclick="window.location.href='../login/logout.php'">
+            <img src="../assets/images/logo.jpg" alt="user" class="userIcon" id="userIcon">
+            <div class="userInfoOverlay" id="userOverlay">
+                <div class="user-container">
+                    <div class="icon">
+                        <img src="../assets/images/logo.jpg" alt="user" class="userIcon">
+                    </div>
+                    <div class="userInfo">
+                        <p class="userName">Frade</p>
+                        <p class="userEmail">frade@gmail.com</p>
+                        <p class="userRole">user</p>
+                    </div>
+                </div>
+                <div class="logoutBtn">
+                    <i class="fi fi-rr-exit" id="logOut"></i>
+                </div>
+            </div>
+
+
+
+            <!-- <img src="../assets/images/logo.jpg" alt="user" class="userIcon" onclick="window.location.href='../login/logout.php'"> -->
         </div>
     </div>
+    <script src="../assets/js/mainScripts.js"></script>
